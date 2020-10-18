7dd7633cfb3fd5ff06f29ca52ad543ef0252e12f
Bogdan <71147719+bkomarow1990@users.noreply.github.com>
2020-10-18 17:27:30 +0300

    Added new menu + functions

--- Student struct/Source.cpp
+++ Student struct/Source.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
 #include <time.h>
 #include <string>
+#include<iostream>
+#include <Windows.h>
+#include <conio.h>
 using namespace std;
+
+void SetColor(int col)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), col);
+}
+
+void Size_Console(int x, int y)
+{
+	HANDLE out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD crd = { x, y };
+	SMALL_RECT src = { 0, 0, crd.X , crd.Y };
+	SetConsoleWindowInfo(out_handle, true, &src);
+	SetConsoleScreenBufferSize(out_handle, crd);
+}
+void SetPos(int x, int y)
+{
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+int menu(string menu_items[], int size) {
+
+	int key = 0;
+	int code;
+	do {
+	/*	cout << "===========================================================================" << endl;
+		cout << "\t\tMENU" << endl;*/
+		system("cls");
+		key = (key + size) % size;
+		for (int i = 0; i < size; i++)
+		{
+	
+			SetPos(20, 10+ i);
+			if (key == i)
+			{
+
+				
+				cout << " "; SetColor(12); cout << menu_items[i]; SetColor(3); cout << " " << endl; SetColor(15);
+			}
+			else
+			{
+				cout << " "; cout << menu_items[i]; cout << " " << endl;
+			}
+
+		}
+		code = _getch();
+		if (code == 224)
+		{
+			code = _getch();
+			if (code == 80) key++;
+			if (code == 72) key--;
+		}
+	} while (code != 13);
+	system("cls");
+	return key;
+}
 struct Date {
 	int day;
 	int month;
@@ -169,6 +230,7 @@ string getClassWithWorstMarks(Teachers* teachers, Students* students, Class* cla
 	return class__[minIndex].name;
 }
 void showRelatives(Students* students) {
+	cout << "=============showRelatives=============" << endl;
 	string Realatives[15];
 	int count=0;
 	for (int i = 0; i < 15; i++)
@@ -187,23 +249,103 @@ void showRelatives(Students* students) {
 		cout << Realatives[i] << endl;
 	}
 }
-void showMenu(int& answer) {
-	cout << "==========================" << endl;
-	cout << "\tMENU" << endl;
-	cout << "1.Show Students" << endl;
-	cout << "2.Show Teachers" << endl;
-	cout << "3.Show Classes" << endl;
-	cout << "4.Show Excelenters" << endl;
-	cout << "5.Show Teacher with best Class marks" << endl;
-	cout << "6.Show Class with worst Class marks" << endl;
-	cout << "7.Show Relatives" << endl;
-	cout << "8.Show Relatives" << endl;
-	cout << "0.Exit" << endl;
-	cin >> answer;
-	cout << "==========================" << endl;
+//void showMenu(int& answer) {
+//	cout << "===========================================================================" << endl;
+//	cout << "\tMENU" << endl;
+//	cout << "1.Show Students" << endl;
+//	cout << "2.Show Teachers" << endl;
+//	cout << "3.Show Classes" << endl;
+//	cout << "4.Show Excelenters" << endl;
+//	cout << "5.Show Teacher with best Class marks" << endl;
+//	cout << "6.Show Class with worst Class marks" << endl;
+//	cout << "7.Show Relatives" << endl;
+//	cout << "8.Show Students what born in one day" << endl;
+//	cout << "9.Show Oldest Student" << endl;
+//	cout << "10.Show Count Student what have >8.5 average marks" << endl;
+//	cout << "0.Exit" << endl;
+//	cin >> answer;
+//}
+void printStudentsBornOneDay(Students* students) {
+	cout << "=============printStudentsBornOneDay=============" << endl;
+	int count = 0;
+	string StudentsOneDayBorn[15];
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 15; j++)
+		{
+			if (students[i].data.day == students[j].data.day && i != j)
+			{
+				StudentsOneDayBorn[count] = students[i].name + " " + students[i].surname + " - " + students[j].name + " " + students[j].surname;
+				count++;
+			}
+		}
+		
+	}
+	for (int i = 0; i < count; i++)
+	{
+		cout<< StudentsOneDayBorn[i] << endl;
+	}
+}
+void getOldestStudent(Students* students) {
+	cout << "============getOldestStudent==============" << endl;
+	float days[15];
+	for (int i = 0; i < 15; i++)
+	{
+		days[i] = 0;
+	}
+	for (int i = 0; i < 15; i++)
+	{
+		days[i] += students[i].data.day;
+		days[i] += students[i].data.month*30;
+		days[i] += students[i].data.year*365.24;
+	}
+	int min = days[0];
+	int minIndex = 0;
+	for (int i = 0; i < 15; i++)
+	{
+		if (days[i]<min)
+		{
+			min = days[i];
+			minIndex = i;
+		}
+	}
+	cout << "OLDEST: " << students[minIndex].name <<" "<<students[minIndex].surname<< endl;
+	cout << "WITH B-DAY : ";
+	students[minIndex].data.print();
+}
+int countOfStudentsWith85PlusMarks(Students* students) {
+	float average[15];
+	float summ[15];
+	int count = 0;
+	for (int i = 0; i < 15; i++)
+	{
+		summ[i] = 0;
+	}
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			summ[i] += students[i].marks[j];
+		}
+		average[i] = summ[i] / 10;
+	}
+	for (int i = 0; i < 15; i++)
+	{
+		if (average[i]>8.5)
+		{
+			count++;
+		}
+	}
+	return count;
 }
 int main() {
 	srand(time(NULL));
+	Size_Console(50, 50);
+	string Menu[] = { "1.Show Students","2.Show Teachers","3.Show Classes","4.Show Excelenters" ,
+	"5.Show Teacher with best Class marks","6.Show Class with worst Class marks",
+	"7.Show Relatives","8.Show Students what born in one day","9.Show Oldest Student","10.Show Count Student what have >8.5 average marks",
+	"0.Exit"
+	};
 	Students* students = new Students[15];
 	Teachers* teachers = new Teachers[3];
 	Class* class__ = new Class[3];
@@ -222,15 +364,14 @@ int main() {
 	{
 		CreateClass(&class__[i],students,teachers,score,names_index);
 	}
-	int answer;
-	do
-	{
-		showMenu(answer);
+	
+	int answer= menu(Menu, size(Menu));
 		switch (answer)
 		{
 		case 0:
 			break;
 		case 1:
+			cout << "=============Students:=============" << endl;
 			for (int i = 0; i < 15; i++)
 			{
 				students[i].print();
@@ -238,6 +379,7 @@ int main() {
 			}
 			break;
 		case 2:
+			cout << "=============Teachers:=============" << endl;
 			for (int i = 0; i < 3; i++)
 			{
 				teachers[i].print();
@@ -245,22 +387,39 @@ int main() {
 			}
 			break;
 		case 3:
+			cout << "=============Classes:=============" << endl;
 			for (int i = 0; i < 3; i++)
 			{
 				class__[i].print();
 			}
 			break;
 		case 4:
+			cout << "=============getExcellenters=============" << endl;
 			getExcellenters(students);
+			break;
 		case 5:
+			cout << "=============getTeacherWithBestMarks=============" << endl;
 			cout << getTeacherWithBestMarks(teachers,students,class__) << endl;
+			break;
 		case 6:
+			cout << "=============getClassWithWorstMarks=============" << endl;
 			cout << getClassWithWorstMarks(teachers, students, class__) << endl;
+			break;
 		case 7:
 			showRelatives(students);
+			break;
+		case 8:
+			printStudentsBornOneDay(students);
+			break;
+		case 9:
+			getOldestStudent(students);
+			break;
+		case 10:
+			cout << "=============countOfStudentsWith8.5PlusMarks=============" << endl;
+			cout << countOfStudentsWith85PlusMarks(students) << endl;
+			break;
 		default:
 			break;
 		}
-	} while (answer != 0);
 	return 0;
 }
